2e3ebf33301cd528cb4e92f02242ac8277d3154e
Luck <133802752+luck0202@users.noreply.github.com>
2024-06-19 20:30:33 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,69 @@
+//relógio
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+
+const relogio = setInterval(function time() {
+  let dateToday = new Date();
+  let hr = dateToday.getHours();
+  let min = dateToday.getMinutes();
+
+  if (hr < 10) hr = '0' + hr;
+  if (min < 10) min = '0' + min;
+
+  horas.textContent = hr;
+  minutos.textContent = min;
+})
+
+//data
+const day = document.getElementById('day');
+const weekday = document.getElementById('weekday');
+const monthyear = document.getElementById('monthyear');
+const year = document.getElementById('year');
+
+let dataAtual = new Date();
+
+//dia
+let dia = dataAtual.getDate();
+
+//dias da semana
+let diaSemana = dataAtual.getDay();
+if (diaSemana == 0) diaSemana = 'Domingo'
+if (diaSemana == 1) diaSemana = 'Segunda-feira'
+if (diaSemana == 2) diaSemana = 'Terça-feira'
+if (diaSemana == 3) diaSemana = 'Quarta-feira'
+if (diaSemana == 4) diaSemana = 'Quinta-feira'
+if (diaSemana == 5) diaSemana = 'Sexta-feira'
+if (diaSemana == 6) diaSemana = 'Sabado'
+
+//mês
+let mes = dataAtual.getMonth();
+
+if (mes == 0) mes = 'janeiro'
+if (mes == 1) mes = 'fevereiro'
+if (mes == 2) mes = 'março'
+if (mes == 3) mes = 'abril'
+if (mes == 4) mes = 'maio'
+if (mes == 5) mes = 'junho'
+if (mes == 6) mes = 'julho'
+if (mes == 7) mes = 'agosto'
+if (mes == 8) mes = 'setembro'
+if (mes == 9) mes = 'outubro'
+if (mes == 10) mes = 'novembro'
+if (mes == 11) mes = 'dezembro'
+
+//ano
+let ano = dataAtual.getFullYear();
+
+weekday.textContent = diaSemana;
+day.textContent = dia;
+monthyear.textContent = mes;
+year.textContent = ano;
+
+document.getElementById("weekday").innerHTML = diaSemana.toDateString();
+document.getElementById("day").innerHTML = dia.toDateString();
+document.getElementById("monthyear").innerHTML = mes.toDateString();
+document.getElementById("year").innerHTML = ano.toDateString();
+
 //Nova cotação - pág novacot
 const quoteForm = document.getElementById('quote-form');
 const quoteIdInput = document.getElementById('quote-id');
@@ -75,12 +141,12 @@ validateZipCodeBtn.addEventListener('click', () => {
 
 
 //solicitação de compra
-const form = document.getElementById('purchase-request-form');
+const formulario = document.getElementById('purchase-request-form');
 const requestNumberDiv = document.getElementById('request-number');
 const today = new Date().toISOString().split('T')[0];
 document.getElementById('request-date').value = today;
 
-form.addEventListener('submit', (e) => {
+formulario.addEventListener('submit', (e) => {
   e.preventDefault();
   const productCode = document.getElementById('product-code').value;
   const quantity = document.getElementById('quantity').value;
@@ -107,9 +173,9 @@ document.getElementById('consult-product-code').addEventListener('click', () => 
   alert('Consulting product code...');
 });
 const suppliersListDiv = document.getElementById('suppliers-list');
-const searchFieldSelect = document.getElementById('search-field');
-const searchInput = document.getElementById('search-input');
-const searchBtn = document.getElementById('search-btn');
+const campoDeSelecao = document.getElementById('search-field');
+const entradaPesquisa = document.getElementById('search-input');
+const botaoPesquisa = document.getElementById('search-btn');
 const newRegistrationBtn = document.getElementById('new-registration-btn');
 
 // Supõe a a lista de fornecedores
@@ -227,65 +293,3 @@ function displayQuotes(quotes) {
 
 displayQuotes(quotes); // exibir todas as cotações inicialmente
 
-//relógio
-const horas = document.getElementById('horas');
-const minutos = document.getElementById('minutos');
-
-const relogio = setInterval(function time() {
-  let dateToday = new Date();
-  let hr = dateToday.getHours();
-  let min = dateToday.getMinutes();
-
-  horas.textContent = hr;
-  minutos.textContent = min;
-})
-
-//data
-const day = document.getElementById('day');
-const weekday = document.getElementById('weekday');
-const monthyear = document.getElementById('monthyear');
-const year = document.getElementById('year');
-
-let dataAtual = new Date();
-
-//dia
-let dia = dataAtual.getDate();
-
-//dias da semana
-let diaSemana = dataAtual.getDay();
-if (diaSemana == 0) diaSemana = 'Domingo'
-if (diaSemana == 1) diaSemana = 'Segunda-feira'
-if (diaSemana == 2) diaSemana = 'Terça-feira'
-if (diaSemana == 3) diaSemana = 'Quarta-feira'
-if (diaSemana == 4) diaSemana = 'Quinta-feira'
-if (diaSemana == 5) diaSemana = 'Sexta-feira'
-if (diaSemana == 6) diaSemana = 'Sabado'
-
-//mês
-let mes = dataAtual.getMonth();
-
-if (mes == 0) mes = 'janeiro'
-if (mes == 1) mes = 'fevereiro'
-if (mes == 2) mes = 'março'
-if (mes == 3) mes = 'abril'
-if (mes == 4) mes = 'maio'
-if (mes == 5) mes = 'junho'
-if (mes == 6) mes = 'julho'
-if (mes == 7) mes = 'agosto'
-if (mes == 8) mes = 'setembro'
-if (mes == 9) mes = 'outubro'
-if (mes == 10) mes = 'novembro'
-if (mes == 11) mes = 'dezembro'
-
-//ano
-let ano = dataAtual.getFullYear();
-
-weekday.textContent = diaSemana;
-day.textContent = dia;
-monthyear.textContent = mes;
-year.textContent = ano;
-
-document.getElementById("weekday").innerHTML = diaSemana.toDateString();
-document.getElementById("day").innerHTML = dia.toDateString();
-document.getElementById("monthyear").innerHTML = mes.toDateString();
-document.getElementById("year").innerHTML = ano.toDateString();
